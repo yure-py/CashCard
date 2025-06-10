492f19d7bcbb45e08b6d1a19d4a20ee801275fa7
yure <yuking08@hotmail.com>
2025-06-10 15:38:38 -0300

    procedure: first test

--- src/test/java/api/academy/cashcard/CashCardApplicationTests.java
+++ src/test/java/api/academy/cashcard/CashCardApplicationTests.java
@@ -3,11 +3,13 @@ package api.academy.cashcard;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class CashCardApplicationTests {
 
     @Test
-    void contextLoads() {
+    void firstTest() {
+        assertThat(42).isEqualTo(42);
     }
-
 }
