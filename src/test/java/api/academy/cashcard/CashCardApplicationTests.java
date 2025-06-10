61f1cb4e87b145e8d33a20a9fae4d13087f73ecf
yure <yuking08@hotmail.com>
2025-06-10 15:27:34 -0300

    projeto incializado

---
+++ src/test/java/api/academy/cashcard/CashCardApplicationTests.java
@@ -0,0 +1,13 @@
+package api.academy.cashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
